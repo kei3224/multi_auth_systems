709a4f4dc6ba88771a3ddd903fcecead3b025c8b
SDO1 Pangasinan ICT Unit <krrtablang_19ac0055@psu.edu.ph>
2023-04-03 11:58:44 +0800

    first commit

---
+++ resources/views/backend/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Design and Developed by ICT Unit.
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2020-2023 <a href="https://www.depedsdo1pangasinan.com/">SDO1 PANGASINAN</a>.</strong> All rights reserved.
+  </footer>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('list_bookcategory', [App\Http\Controllers\backend\BookcategoryController::class, 'BookCategoryList'])->name('bookcategory.index');
+Route::get('/add_bookcategory', [App\Http\Controllers\backend\BookcategoryController::class, 'BookCategoryAdd'])->name('bookcategoryadd');
+Route::post('/insert_bookcategory', [App\Http\Controllers\backend\BookcategoryController::class, 'BookCategoryInsert']);
+Route::get('/edit_bookcategory/{id}', [App\Http\Controllers\backend\BookcategoryController::class, 'BookEditCategory']);
+Route::post('/update_bookcategory/{id}', [App\Http\Controllers\backend\BookcategoryController::class, 'BookUpdateCategory']);
+Route::get('/delete_bookcategory/{id}', [App\Http\Controllers\backend\BookcategoryController::class, 'BookDeleteCategory']);
+
+Route::get('user_list', [App\Http\Controllers\backend\UsermanagementController::class, 'UserList'])->name('user.index');
+Route::get('/edit_user/{id}', [App\Http\Controllers\backend\UsermanagementController::class, 'UserEdit']);
+Route::post('/update_user/{id}', [App\Http\Controllers\backend\UsermanagementController::class, 'UserUpdate']);
+Route::get('/delete_user/{id}', [App\Http\Controllers\backend\UsermanagementController::class, 'UserDelete']);
